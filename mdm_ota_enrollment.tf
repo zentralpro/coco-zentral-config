ea9bcc972f2a8714eb0a435a7964dc776ada5653
sebastian <sebastian@zentral.com>
2026-09-16 12:35:14 +0200

    Add mobile blueprint and artifacts

--- mdm_ota_enrollment.tf
+++ mdm_ota_enrollment.tf
@@ -14,3 +14,12 @@ resource "zentral_mdm_ota_enrollment" "default" {
   scep_issuer_id        = data.zentral_mdm_scep_issuer.zentral-cloud.id
   meta_business_unit_id = zentral_meta_business_unit.default.id
 }
+
+resource "zentral_mdm_ota_enrollment" "mobile" {
+  name                  = "Mobile"
+  display_name          = "Mobile - Zentral Cloud"
+  blueprint_id          = zentral_mdm_blueprint.mobile.id
+  push_certificate_id   = data.zentral_mdm_push_certificate.zentral-cloud.id
+  scep_issuer_id        = data.zentral_mdm_scep_issuer.zentral-cloud.id
+  meta_business_unit_id = zentral_meta_business_unit.default.id
+}
