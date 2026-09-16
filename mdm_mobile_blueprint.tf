285afb3c0106b2b169c14e4defe354e059c40667
sebastian <sebastian@zentral.com>
2026-09-16 14:09:25 +0200

    Add default location

--- mdm_mobile_blueprint.tf
+++ mdm_mobile_blueprint.tf
@@ -3,8 +3,14 @@ resource "zentral_mdm_blueprint" "mobile" {
   collect_apps            = "ALL"
   collect_certificates    = "ALL"
   collect_profiles        = "ALL"
+  default_location_id     = data.zentral_mdm_location.default.id
   legacy_profiles_via_ddm = true
 }
+
+data "zentral_mdm_location" "default" {
+  name = "petit-coco.zentral.cloud"
+}
+
 
 # Mobile
 
